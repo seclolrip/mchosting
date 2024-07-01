17164165bfe3798a300f4a608d91cfda0691fe36
seclolrip <derrick.k.karlinsey@outlook.com>
2024-06-30 22:31:11 -0600

    pre-validation

---
+++ hardserv/start.go
@@ -0,0 +1,5 @@
+package start
+
+// Args:
+// Java Version, Min Mem, Max Mem, Log4j workaround, any command args needed for modded worlds including multiple jar files for forge or spigot
+// Garbage collection commands, optimizations, etc
